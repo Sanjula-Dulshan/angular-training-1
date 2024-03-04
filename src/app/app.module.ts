0fbb457faf5cf19a4a8b83c02500acd39db5f31c
Sanjula-Dulshan <sdulshan10@gmail.com>
2024-03-04 15:14:09 +0530

    configure routes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,21 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule, Routes } from '@angular/router'; // Import RouterModule here
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { UserComponent } from './user/user.component';
 
+const routes: Routes = [
+  { path: 'home', component: HomeComponent },
+  { path: 'user', component: UserComponent },
+  { path: '', redirectTo: '/home', pathMatch: 'full' }, // Redirect to home component by default
+];
 @NgModule({
-  declarations: [
-    AppComponent,
-    HomeComponent,
-    UserComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  declarations: [AppComponent, HomeComponent, UserComponent],
+  imports: [BrowserModule, AppRoutingModule, RouterModule.forRoot(routes)],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
